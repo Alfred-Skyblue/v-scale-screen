a30673bec05853beb5cb20770e2cde9a52ea7fea
yangpanteng <893042011@qq.com>
2022-04-14 00:42:53 +0800

    docs: 写文档

--- src/entry.esm.ts
+++ src/entry.esm.ts
@@ -8,9 +8,7 @@ import component from '@/v-scale-screen.vue';
 // Define typescript interfaces for installable component
 type InstallableComponent = typeof component & PluginObject<any>;
 
-// Default export is installable instance of component.
-// IIFE injects install function into component, allowing component
-// to be registered via Vue.use() as well as Vue.component(),
+
 export default /*#__PURE__*/((): InstallableComponent => {
   // Assign InstallableComponent type
   const installable = component as unknown as InstallableComponent;
